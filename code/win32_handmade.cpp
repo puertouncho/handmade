5290804bf8a52f9aad4c83b0361ad2e0b07810a8
Puertouncho <puertouncho@gmx.es>
2017-06-10 17:25:10 +0100

    Opening First Window - Warning not close functionality yet

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -7,12 +7,128 @@
 
 #include <windows.h>
 
-int CALLBACK WinMain( HINSTANCE hInstance,
-  					  HINSTANCE hPrevInstance,
-  					  LPSTR     lpCmdLine,
-  					  int       nCmdShow) 
+
+LRESULT CALLBACK
+MainWindowCallback(HWND   Window,
+				   UINT   Message,
+				   WPARAM WParam,
+				   LPARAM LParam )
 {
-	MessageBoxExA(0, "Hand Made Hero","Handmade Title", 
-				  MB_OK|MB_ICONINFORMATION, 0 );
+	LRESULT Result = 0;
+
+	switch(Message)
+	{
+		case WM_SIZE:
+		{
+			OutputDebugStringA("WM_SIZE\n");
+		}
+		break;
+
+		case WM_DESTROY:
+		{
+			OutputDebugStringA("WH_DESTROY\n");
+		}
+		break;
+
+		case WM_CLOSE:
+		{
+			OutputDebugStringA("WH_CLOSE\n");
+		}
+		break;
+
+		case WM_PAINT:
+		{
+			OutputDebugStringA("WM_PAINT\n");
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+
+			int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.bottom;
+			LONG Height = Paint.rcPaint.bottom - Paint.rcPaint.top; 
+			LONG Width  = Paint.rcPaint.right - Paint.rcPaint.left; 
+
+			static DWORD Operation = WHITENESS;
+			if(Operation == WHITENESS)
+			{
+				Operation = BLACKNESS;
+			}
+			else
+			{
+				Operation = WHITENESS;
+			}
+			PatBlt(DeviceContext,X, Y, Width, Height, Operation);
+
+			EndPaint(Window, &Paint);
+		}
+		break;
+
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugStringA("WH_ACTIVATEAPP\n");
+		}
+		break;
+
+		default:
+		{
+			//OutputDebugStringA("default\n");
+			Result = DefWindowProc( Window,Message,WParam,LParam);
+		}
+		break;
+	}
+	return Result;
+}
+
+int CALLBACK WinMain( HINSTANCE Instance,
+  					  HINSTANCE PrevInstance,
+  					  LPSTR     CmdLine,
+  					  int       CmdShow) 
+{
+	WNDCLASS WindowClass = {};
+	WindowClass.style 			= CS_OWNDC;
+	WindowClass.lpfnWndProc 	= MainWindowCallback;
+	WindowClass.hInstance 		= Instance;
+	WindowClass.lpszClassName 	= "HandmadeHeroWindowClass"; 
+
+	if(RegisterClassA(&WindowClass))
+	{
+		HWND WindowHandle = CreateWindowExA(0,
+								  		    WindowClass.lpszClassName,
+								  		    "Handmade Hero",
+								  		    WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+								  		    CW_USEDEFAULT,
+								 		    CW_USEDEFAULT,
+								  		    CW_USEDEFAULT,
+								  		    CW_USEDEFAULT,
+								 		    0,
+								  		    0,
+								  		    Instance,
+								  		    0);
+		if(WindowHandle)
+		{
+			MSG Message;
+			while(true)
+			{
+				BOOL MessageResult = GetMessageA(&Message,0,0,0);
+				if(MessageResult >0)
+				{
+					TranslateMessage(&Message);
+					DispatchMessageA(&Message);
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+			// TODO: Logging
+		}
+	}
+	else
+	{
+		// TODO: Logging
+	}
+
 	return (0);
 }	
